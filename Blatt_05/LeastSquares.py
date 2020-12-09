3eed41639d9dc2babe4a661d087f0458c3362199
InuGatoah <44636958+InuGatoah@users.noreply.github.com>
2020-12-09 23:44:18 +0100

    Create LeastSquares.py

---
+++ Blatt_05/LeastSquares.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def QR(A):
+    """Given a matrix A with full column rank this function uses the classical 
+       Gram-Schmidt algorithm to compute a QR decomposition. It returns a tuple 
+       (Q,R) of np.matrix objects with Q having shape identical to A and Q*R=A."""
+
+
+def BackSubstitution(R: np.matrix, y: np.ndarray):
+    """Given a square upper triangular matrix R and a vector y of same size this 
+       function solves R*x=y using backward substitution and returns x."""
+
+
+def LeastSquares(A, b):
+    """Given a matrix A and a vector b this function solves the least squares 
+       problem of minimizing |A*x-b| and returns the optimal x."""
+
+
+if(__name__ == "__main__"):
+    A = np.random.rand(13, 10) * 1000
+    # Try the QR decomposition
+    A = np.matrix([
+        [1.0, 1.0, 1.0],
+        [1.0, 2.0, 3.0],
+        [1.0, 4.0, 9.0],
+        [1.0, 8.0, 27.0]
+    ])
+    Q, R = QR(A)
+    print("If the following numbers are nearly zero, QR seems to be working.")
+    print(np.linalg.norm(Q*R-A))
+    print(np.linalg.norm(Q.H*Q-np.eye(3)))
+    # Try solving a least squares system
+    b = np.matrix([1.0, 2.0, 3.0, 4.0]).T
+    x = LeastSquares(A, b)
+    print("If the following number is nearly zero, least squares solving seems to be working.")
+    print(np.linalg.norm(x-np.linalg.lstsq(A, b)[0]))
