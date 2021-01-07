1237d4fc48f3395615b2e52945ce543fc786ebd6
InuGatoah <programming.github@sent.at>
2021-01-07 23:49:38 +0100

    Worked on LeastSquares.py

--- Blatt_05/LeastSquares.py
+++ Blatt_05/LeastSquares.py
@@ -6,15 +6,16 @@ def QR(A):
        Gram-Schmidt algorithm to compute a QR decomposition. It returns a tuple 
        (Q,R) of np.matrix objects with Q having shape identical to A and Q*R=A."""
     
+    V = A.copy()
     n, m = A.shape
     Q = np.zeros((n, m))
     R = np.zeros((m, m))
     
     for j in range(m):
-        R[j, j] = np.linalg.norm(A[:, j:j+1].reshape(-1), 2)
-        Q[:, j:j+1] = A[:, j:j+1]/R[j, j]
-        R[j:j+1, j+1:m+1] = np.dot(Q[:, j:j+1].T, A[:, j+1:m+1])
-        A[:, j+1:m+1] = A[:, j+1:m+1] - np.dot(Q[:, j:j+1], R[j:j+1, j+1:m+1])
+        R[j, j] = np.linalg.norm(V[:, j:j+1].reshape(-1), 2)
+        Q[:, j:j+1] = V[:, j:j+1]/R[j, j]
+        R[j:j+1, j+1:m+1] = np.dot(Q[:, j:j+1].T, V[:, j+1:m+1])
+        V[:, j+1:m+1] = V[:, j+1:m+1] - np.dot(Q[:, j:j+1], R[j:j+1, j+1:m+1])
         
     return np.asmatrix(Q), np.asmatrix(R)     
 
@@ -23,12 +24,23 @@ def BackSubstitution(R: np.matrix, y: np.ndarray):
     """Given a square upper triangular matrix R and a vector y of same size this 
        function solves R*x=y using backward substitution and returns x."""
     
-    n, m = R.shape
-    x = np.zeros_like(y)
+    n = R.shape[1]
+    x = np.zeros((n, 1), dtype = np.dtype)
     
-    for i in range(m):
-        x[i] = y[i] / R[i, i] 
-        y[1:i-1] = y[1:i-1, i] * x[i]
+    #Backward substitution
+    for i in range(n-1, -1, -1):
+        x[i] = y[i]
+        for j in range(i+1, n):
+            x[i] -= x[j] * R[i, j]
+        x[i] /= R[i,i]
+
+    #old code
+    #n, m = R.shape
+    #x = np.zeros_like(y)
+    #
+    #for i in range(m):
+    #    x[i] = y[i] / R[i, i] 
+    #    y[1:i-1] = y[1:i-1, i] * x[i]
         
     return x
 
