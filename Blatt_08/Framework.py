2c57e2c13bf59440f9aa468956277d2cca527e26
InuGatoah <programming.github@sent.at>
2021-01-12 23:30:10 +0100

    added Blatt_08

---
+++ Blatt_08/Framework.py
@@ -0,0 +1,51 @@
+import numpy as np
+  
+
+def LUP(A):
+    """Computes and returns an LU decomposition with pivoting. The return value  
+       is a tuple (L,U,P) and fulfills L*U=P*A (* is matrix-multiplication)."""
+    
+
+def ForwardSubstitution(L,b):
+    """Solves the linear system of equations L*x=b assuming that L is a left lower 
+       triangular matrix. It returns x as column vector."""
+    
+
+
+def BackSubstitution(U,b):
+    """Solves the linear system of equations U*x=b assuming that U is a right upper 
+       triangular matrix. It returns x as column vector."""
+    
+	
+def SolveLinearSystemLUP(A,b):
+    """Given a square array A and a matching vector b this function solves the 
+       linear system of equations A*x=b using a pivoted LU decomposition and returns 
+       x."""
+    
+
+def LeastSquares(A,b):
+    """Given a matrix A and a vector b this function solves the least squares 
+       problem of minimizing |A*x-b| and returns the optimal x."""
+    
+
+if(__name__=="__main__"):
+    # A test matrix where LU fails but LUP works fine
+    A=np.array([[1,2, 6],
+                [4,8,-1],
+                [2,3, 5]],dtype=np.double);
+    b=np.array([[1],[2],[3]],dtype=np.double);
+    # Test the LUP-decomposition
+    L,U,P=LUP(A)
+    print("L")
+    print(L)
+    print("U")
+    print(U)
+    print("P")
+    print(P)
+    print("Zero (LUP sanity check): "+str(np.linalg.norm(np.dot(L,U)-np.dot(P,A))))
+    # Test the method for solving a system of linear equations
+    print("Zero (SolveLinearSystemLUP sanity check): "+str(np.linalg.norm(np.dot(A,SolveLinearSystemLUP(A,b))-b)))
+    # Test the method for solving linear least squares
+    A=np.random.rand(6,4)
+    b=np.random.rand(6)
+    print("Zero (LeastSquares sanity check): "+str(np.linalg.norm(LeastSquares(A,b).flat-np.linalg.lstsq(A,b)[0])))
