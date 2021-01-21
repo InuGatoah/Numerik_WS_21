a5c229417e8765013505f6c0dede7b753cb0247e
InuGatoah <programming.github@sent.at>
2021-01-21 22:27:45 +0100

    worked on Blatt_09

--- Blatt_09/FrameworkEigenvalue.py
+++ Blatt_09/FrameworkEigenvalue.py
@@ -2,12 +2,25 @@ import scipy.io
 import numpy as np
 from matplotlib import pyplot
 
+i = 100
+
 def PowerIteration(A,v):
     """Given a square matrix A and a column vector v of matching size to initialize 
        the iteration this function implements the power iteration to approximate the 
        largest Eigenvalue of A. It returns a list of successive approximations where 
        the last approximation is the best one."""
 
+    result = []
+    w = v
+    Av = A.dot(w)
+
+    for j in range(i):
+        w = Av / np.linalg.norm(Av)
+        Av = A.dot(w)
+        delta = w.flatten().dot(Av)
+        result.append(delta)
+
+    return result
 
 def RayleighQuotientIteration(A,v):
     """Given a square matrix A and a column vector v of matching size to initialize 
@@ -16,6 +29,16 @@ def RayleighQuotientIteration(A,v):
        a list of successive approximations where the last approximation is the best 
        one."""
 
+    result = []
+    w = v
+    Av = A.dot(w)
+    delta = np.eye(A.shape[0])
+
+    for j in range(i):
+
+        result
+
+    return result
 
 Matrizen=scipy.io.loadmat("Matrizen.mat");
 A1=Matrizen["A1"];
